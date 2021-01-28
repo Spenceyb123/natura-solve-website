4c6c91930e40af754fabf2821a4741ff1ea14e9d
Spenceyb123 <41405434+Spenceyb123@users.noreply.github.com>
2021-01-28 07:58:53 -0700

    Add files via upload
    got main buy page done (mostly), uploading to vercel now and then working on individual product pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,22 +5,22 @@ let mine = document.getElementsByClassName('tile-img mine')[0];
 
 let mosquito = document.getElementsByClassName('tile-img mosquito')[0];
 
-window.innerWidth > window.innerHeight ? wastewater.src = "natural-wastewater-treatment-landscape.png" : wastewater.src = "natural-wastewater-treatment-mobile.png";
+window.innerWidth > window.innerHeight ? wastewater.src = "homepage/natural-wastewater-treatment-landscape.png" : wastewater.src = "homepage/natural-wastewater-treatment-mobile.png";
     
-window.innerWidth > window.innerHeight ? mine.src = "mine-remediation-landscape.png" : mine.src = "mine-remediation-mobile.png";
+window.innerWidth > window.innerHeight ? mine.src = "homepage/mine-remediation-landscape.png" : mine.src = "homepage/mine-remediation-mobile.png";
 
-window.innerWidth > window.innerHeight ? mosquito.src = "mosquito-control-natural-landscape.png" : mosquito.src = "mosquito-control-natural-mobile.png";
+window.innerWidth > window.innerHeight ? mosquito.src = "homepage/mosquito-control-natural-landscape.png" : mosquito.src = "homepage/mosquito-control-natural-mobile.png";
 
-window.innerWidth > window.innerHeight ? mosquito.src = "mosquito-control-natural-landscape.png" : mosquito.src = "mosquito-control-natural-mobile.png";
+window.innerWidth > window.innerHeight ? mosquito.src = "homepage/mosquito-control-natural-landscape.png" : mosquito.src = "homepage/mosquito-control-natural-mobile.png";
 
     
 
 function reportWindowSize() {
-  window.innerWidth > window.innerHeight ? wastewater.src = "natural-wastewater-treatment-landscape.png" : wastewater.src = "natural-wastewater-treatment-mobile.png";
+  window.innerWidth > window.innerHeight ? wastewater.src = "homepage/natural-wastewater-treatment-landscape.png" : wastewater.src = "homepage/natural-wastewater-treatment-mobile.png";
     
-  window.innerWidth > window.innerHeight ? mine.src = "mine-remediation-landscape.png" : mine.src = "mine-remediation-mobile.png"; 
+  window.innerWidth > window.innerHeight ? mine.src = "homepage/mine-remediation-landscape.png" : mine.src = "homepage/mine-remediation-mobile.png"; 
     
-    window.innerWidth > window.innerHeight ? mosquito.src = "mosquito-control-natural-landscape.png" : mosquito.src = "mosquito-control-natural-mobile.png";
+    window.innerWidth > window.innerHeight ? mosquito.src = "homepage/mosquito-control-natural-landscape.png" : mosquito.src = "homepage/mosquito-control-natural-mobile.png";
 }
 
 window.onresize = reportWindowSize;
